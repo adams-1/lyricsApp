31eb051fc92c8602da07bb6b8b8fa0752d73a417
rage <obliviousness@gmail.com>
2020-01-11 11:14:45 +0200

    moving files and dirs

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Header.css';
+
+
+function Header() {
+
+    return (
+        <div>
+          <header className="Header">
+            <p>
+              Lyrics App - The Best of All.
+            </p>
+          </header>
+        </div>
+    );
+}
+
+export default Header;
